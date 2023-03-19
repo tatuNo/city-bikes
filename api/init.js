b16b978a341f43732e595bdfb12d6b517a5b915c
Tatu <tatunordstrom@gmail.com>
2023-03-19 14:35:39 +0200

    validate csv line

--- api/init.js
+++ api/init.js
@@ -2,7 +2,35 @@ const fs = require("fs");
 const path = require("path");
 const csv = require("fast-csv");
 
-const validateLine = (data) => true;
+const isValidDate = (dateString) => {
+  const date = new Date(dateString);
+  return date instanceof Date && !Number.isNaN(date);
+};
+
+const validateLine = (line) => {
+  const {
+    depatureDate,
+    returnDate,
+    depatureStationId,
+    returnStationId,
+    distance,
+    duration,
+  } = line;
+
+  if (!isValidDate(depatureDate) || !isValidDate(returnDate)) {
+    return false;
+  }
+
+  if (depatureStationId <= 0 || returnStationId <= 0) {
+    return false;
+  }
+
+  if (distance <= 10 || duration <= 10) {
+    return false;
+  }
+
+  return true;
+};
 
 fs.createReadStream(path.resolve(__dirname, "data", "2021-05.csv"))
   .pipe(
