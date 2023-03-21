dcf972bcdad494804a2907c4aef094c8c8e2bce6
Tatu <tatunordstrom@gmail.com>
2023-03-21 17:19:55 +0200

    transform datatypes before validation

--- api/init.js
+++ api/init.js
@@ -5,6 +5,8 @@ const csv = require("fast-csv");
 const { validateLine } = require("./utils/validation");
 const { connectToDatabase } = require("./utils/db");
 
+const validData = [];
+
 fs.createReadStream(path.resolve(__dirname, "data", "2021-05.csv"))
   .pipe(
     csv.parse({
@@ -21,9 +23,22 @@ fs.createReadStream(path.resolve(__dirname, "data", "2021-05.csv"))
       ],
     })
   )
+  .transform((data) => ({
+    ...data,
+    depatureDate: new Date(data.depatureDate),
+    returnDate: new Date(data.returnDate),
+    depatureStationId: Number(data.depatureStationId),
+    returnStationId: Number(data.returnStationId),
+    distance: Number(data.distance),
+    duration: Number(data.duration),
+  }))
   .validate((data) => validateLine(data))
   .on("error", (error) => console.error(error))
-  .on("data-invalid", (row, rowNumber) => console.dir(row));
+  .on("data", (data) => validData.push(data))
+  .on("data-invalid", (row, rowNumber) => {
+    console.log("RIVI", rowNumber);
+    console.dir(row);
+  });
 
 const start = async () => {
   await connectToDatabase();
