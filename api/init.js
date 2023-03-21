cd30ec3f4be2584a5b99b628ad5f576f9a24e16d
Tatu <tatunordstrom@gmail.com>
2023-03-21 18:20:36 +0200

    push valid data to database

--- api/init.js
+++ api/init.js
@@ -4,44 +4,45 @@ const csv = require("fast-csv");
 
 const { validateLine } = require("./utils/validation");
 const { connectToDatabase } = require("./utils/db");
+const { Journey } = require("./models");
 
 const validData = [];
 
-fs.createReadStream(path.resolve(__dirname, "data", "2021-05.csv"))
-  .pipe(
-    csv.parse({
-      renameHeaders: true,
-      headers: [
-        "depatureDate",
-        "returnDate",
-        "depatureStationId",
-        "depatureStation",
-        "returnStationId",
-        "returnStation",
-        "distance",
-        "duration",
-      ],
-    })
-  )
-  .transform((data) => ({
-    ...data,
-    depatureDate: new Date(data.depatureDate),
-    returnDate: new Date(data.returnDate),
-    depatureStationId: Number(data.depatureStationId),
-    returnStationId: Number(data.returnStationId),
-    distance: Number(data.distance),
-    duration: Number(data.duration),
-  }))
-  .validate((data) => validateLine(data))
-  .on("error", (error) => console.error(error))
-  .on("data", (data) => validData.push(data))
-  .on("data-invalid", (row, rowNumber) => {
-    console.log("RIVI", rowNumber);
-    console.dir(row);
-  });
+const initDatabase = () => {
+  fs.createReadStream(path.resolve(__dirname, "data", "2021-05.csv"))
+    .pipe(
+      csv.parse({
+        renameHeaders: true,
+        headers: [
+          "depatureDate",
+          "returnDate",
+          "depatureStationId",
+          "depatureStation",
+          "returnStationId",
+          "returnStation",
+          "distance",
+          "duration",
+        ],
+      })
+    )
+    .transform((data) => ({
+      ...data,
+      depatureDate: new Date(data.depatureDate),
+      returnDate: new Date(data.returnDate),
+      depatureStationId: Number(data.depatureStationId),
+      returnStationId: Number(data.returnStationId),
+      distance: Number(data.distance),
+      duration: Number(data.duration),
+    }))
+    .validate((data) => validateLine(data))
+    .on("error", (error) => console.error(error))
+    .on("data", (data) => validData.push(data))
+    .on("end", () => Journey.bulkCreate(validData));
+};
 
 const start = async () => {
   await connectToDatabase();
+  initDatabase();
 };
 
 start();
