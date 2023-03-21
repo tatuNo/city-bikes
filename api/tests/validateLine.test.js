013d95e05c8601745cc8059ead63e79a7d1934c7
Tatu <tatunordstrom@gmail.com>
2023-03-21 17:30:54 +0200

    update validateLine tests

--- api/tests/validateLine.test.js
+++ api/tests/validateLine.test.js
@@ -3,84 +3,84 @@ const { validateLine } = require("../utils/validation");
 describe("validateLine", () => {
   test("should return true when all values are valid", () => {
     const lineObject = {
-      depatureDate: "2021-05-31T23:49:59",
-      returnDate: "2021-05-31T23:55:38",
-      depatureStationId: "001",
-      returnStationId: "002",
-      distance: "20",
-      duration: "100",
+      depatureDate: new Date("2021-05-31T23:49:59"),
+      returnDate: new Date("2021-05-31T23:55:38"),
+      depatureStationId: 1,
+      returnStationId: 2,
+      distance: 20,
+      duration: 100,
     };
     expect(validateLine(lineObject)).toBe(true);
   });
 
   test("should return false when depature date is invalid", () => {
     const lineObject = {
-      depatureDate: "invalid date string",
-      returnDate: "2021-05-31T23:55:38",
-      depatureStationId: "001",
-      returnStationId: "002",
-      distance: "20",
-      duration: "100",
+      depatureDate: new Date("invalid date string"),
+      returnDate: new Date("2021-05-31T23:55:38"),
+      depatureStationId: 1,
+      returnStationId: 2,
+      distance: 20,
+      duration: 100,
     };
     expect(validateLine(lineObject)).toBe(false);
   });
 
   test("should return false when return date is invalid", () => {
     const lineObject = {
-      depatureDate: "2021-05-31T23:49:59",
-      returnDate: "invalid",
-      depatureStationId: "001",
-      returnStationId: "002",
-      distance: "20",
-      duration: "100",
+      depatureDate: new Date("2021-05-31T23:49:59"),
+      returnDate: new Date("invalid"),
+      depatureStationId: 1,
+      returnStationId: 2,
+      distance: 20,
+      duration: 100,
     };
     expect(validateLine(lineObject)).toBe(false);
   });
 
   test("should return false when depature station id is less than 0", () => {
     const lineObject = {
-      depatureDate: "2021-05-31T23:49:59",
-      returnDate: "2021-05-31T23:55:38",
-      depatureStationId: "-1",
-      returnStationId: "002",
-      distance: "20",
-      duration: "100",
+      depatureDate: new Date("2021-05-31T23:49:59"),
+      returnDate: new Date("2021-05-31T23:55:38"),
+      depatureStationId: -1,
+      returnStationId: 2,
+      distance: 20,
+      duration: 100,
     };
     expect(validateLine(lineObject)).toBe(false);
   });
 
   test("should return false when return station id is less than 0", () => {
     const lineObject = {
-      depatureDate: "123",
-      returnDate: "2021-05-31T23:55:38",
-      depatureStationId: "001",
-      returnStationId: "-1",
-      distance: "20",
-      duration: "100",
+      depatureDate: new Date("2021-05-31T23:49:59"),
+      returnDate: new Date("2021-05-31T23:55:38"),
+      depatureStationId: 1,
+      returnStationId: -1,
+      distance: 20,
+      duration: 100,
     };
     expect(validateLine(lineObject)).toBe(false);
   });
 
   test("should return false when distance is less than 10", () => {
     const lineObject = {
-      depatureDate: "2021-05-31T23:49:59",
-      returnDate: "2021-05-31T23:55:38",
-      depatureStationId: "001",
-      returnStationId: "002",
-      distance: "9",
-      duration: "100",
+      depatureDate: new Date("2021-05-31T23:49:59"),
+      returnDate: new Date("2021-05-31T23:55:38"),
+      depatureStationId: 1,
+      returnStationId: 2,
+      distance: 9,
+      duration: 100,
     };
     expect(validateLine(lineObject)).toBe(false);
   });
 
   test("should return false when duration is less than 10", () => {
     const lineObject = {
-      depatureDate: "2021-05-31T23:49:59",
-      returnDate: "2021-05-31T23:55:38",
-      depatureStationId: "001",
-      returnStationId: "002",
-      distance: "9",
-      duration: "100",
+      depatureDate: new Date("2021-05-31T23:49:59"),
+      returnDate: new Date("2021-05-31T23:55:38"),
+      depatureStationId: 1,
+      returnStationId: 2,
+      distance: 9,
+      duration: 100,
     };
     expect(validateLine(lineObject)).toBe(false);
   });
