fa782939b9de0c61f7e8caec66b9f1816e623dd3
Tatu <tatunordstrom@gmail.com>
2023-03-22 13:19:50 +0200

    install tailwind

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 80,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  endOfLine: "lf",
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
-const App = () => <div>Hello</div>;
+const App = () => (
+  <div>
+    <h1 className="text-center text-3xl font-bold text-red-500 underline">
+      Hello world!
+    </h1>
+  </div>
+);
 
 export default App;
