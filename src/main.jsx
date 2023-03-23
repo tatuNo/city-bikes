67b69db60f52db2d907cf2697ef2f18f10a58458
Tatu <tatunordstrom@gmail.com>
2023-03-23 17:14:53 +0200

    install axios, react-query

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 // eslint-disable-next-line no-undef
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
