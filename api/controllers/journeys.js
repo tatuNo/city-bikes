9c721252852d0d1e81195ac2c9b979527e5b6c6a
Tatu <tatunordstrom@gmail.com>
2023-04-03 12:11:46 +0300

    journey distance, duration filter. station search

--- api/controllers/journeys.js
+++ api/controllers/journeys.js
@@ -1,14 +1,58 @@
+const { Op } = require("sequelize");
 const router = require("express").Router();
 
 const { Journey } = require("../models");
 
 router.get("/", async (req, res) => {
-  const { limit = 10, offset = 0, sort = "id" } = req.query;
+  const {
+    limit = 10,
+    offset = 0,
+    sort = "id",
+    distance,
+    duration,
+    station,
+  } = req.query;
+
+  let where = {};
+
+  if (distance) {
+    const [min, max] = distance.split(",").map(parseFloat);
+    where.distance = { [Op.between]: [min, max] };
+  }
+
+  if (duration) {
+    const [min, max] = distance.split(",").map(parseFloat);
+    where.distance = { [Op.between]: [min, max] };
+  }
+
+  if (station) {
+    where = {
+      ...where,
+      [Op.or]: [
+        {
+          returnStation: {
+            [Op.iLike]: `%${station}%`,
+          },
+        },
+        {
+          depatureStation: {
+            [Op.iLike]: `%${station}%`,
+          },
+        },
+      ],
+    };
+  }
+
   const order = sort.startsWith("-")
     ? [[sort.slice(1), "DESC"]]
     : [[sort, "ASC"]];
 
-  const journeys = await Journey.findAndCountAll({ limit, offset, order });
+  const journeys = await Journey.findAndCountAll({
+    limit,
+    offset,
+    order,
+    where,
+  });
   res.json(journeys);
 });
 
