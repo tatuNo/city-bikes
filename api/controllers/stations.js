ab5d0f4c1244b71f957434a0cb72adcc6c1c71ad
Tatu <tatunordstrom@gmail.com>
2023-04-07 11:24:31 +0300

    pagination, search for stations

--- api/controllers/stations.js
+++ api/controllers/stations.js
@@ -4,7 +4,27 @@ const router = require("express").Router();
 const { Station } = require("../models");
 
 router.get("/", async (req, res) => {
-  const stations = await Station.findAndCountAll({});
+  const { limit = 10, offset = 0, search } = req.query;
+  let where = {};
+
+  if (search) {
+    where = {
+      [Op.or]: [
+        {
+          name: {
+            [Op.iLike]: `%${search}%`,
+          },
+        },
+        {
+          address: {
+            [Op.iLike]: `%${search}%`,
+          },
+        },
+      ],
+    };
+  }
+
+  const stations = await Station.findAndCountAll({ limit, offset, where });
   res.json(stations);
 });
 
