b9c686eec8027fe32a38a66135841063ee028c92
Tatu <tatunordstrom@gmail.com>
2023-05-11 16:59:46 +0300

    test e2e stations filter

--- cypress/e2e/stations.cy.js
+++ cypress/e2e/stations.cy.js
@@ -1,7 +1,23 @@
+const setInput = (filterName, value) => {
+  cy.get(`input[name="${filterName}"]`).type(value).should("have.value", value);
+};
+
 describe("Stations", function () {
   beforeEach(function () {
     cy.visit("http://localhost:5173/stations");
   });
 
-  
+  it("can be filtered", function () {
+    const keyword = "kam";
+    setInput("search", keyword);
+    cy.wait(1000);
+    cy.get("#stations > tr").each((row) => {
+      const firstTdText = row.find("td").eq(0).text().toLowerCase();
+      const secondTdText = row.find("td").eq(1).text().toLowerCase();
+
+      cy.wrap(
+        firstTdText.includes(keyword) || secondTdText.includes(keyword)
+      ).should("be.true");
+    });
+  });
 });
